6d4ac4dd5fe18e03ad57a1667d0cdebf7ca52aa0
war-turtle <kartikyadav71199@gmail.com>
2018-09-29 14:30:13 -0700

    boad and star animation added

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 window.addEventListener("DOMContentLoaded", function() {
     calculateLightHousePosition();
+    boatAnimation();
+    startAnimation();
 });
 
 function calculateLightHousePosition() {
@@ -11,4 +13,32 @@ function calculateLightHousePosition() {
         .getBoundingClientRect().top;
     document.getElementById("lighthouseSvg").style.top =
         seaTop - lightHouse1Top;
+}
+
+function boatAnimation() {
+    var boat = document.getElementById("boat");
+    TweenMax.to(boat, 10, { left: "10%", ease: Power3.easeOut, delay: 5 });
+    var time = new TimelineMax({ repeat: -1, delay: 15 });
+    time.to(boat, 5, { rotationZ: "-=3deg" });
+    time.to(boat, 5, { rotationZ: "+=3deg" });
+}
+
+function startAnimation() {
+    var time = new TimelineMax({ repeat: -1 });
+    time.staggerTo("#star11 > path", 0.5, { opacity: 0 }, 0.05, "0");
+    time.staggerTo("#star11 > path", 0.5, { opacity: 1 }, 0.05, "1");
+    time.staggerTo("#star12 > path", 0.5, { opacity: 0 }, 0.05, "0");
+    time.staggerTo("#star12 > path", 0.5, { opacity: 1 }, 0.05, "1");
+    time.staggerTo("#star21 > path", 0.5, { opacity: 0 }, 0.05, "0");
+    time.staggerTo("#star21 > path", 0.5, { opacity: 1 }, 0.05, "1");
+    time.staggerTo("#star22 > path", 0.5, { opacity: 0 }, 0.05, "0");
+    time.staggerTo("#star22 > path", 0.5, { opacity: 1 }, 0.05, "1");
+    time.staggerTo("#star31 > path", 0.5, { opacity: 0 }, 0.05, "0");
+    time.staggerTo("#star31 > path", 0.5, { opacity: 1 }, 0.05, "1");
+    time.staggerTo("#star32 > path", 0.5, { opacity: 0 }, 0.05, "0");
+    time.staggerTo("#star32 > path", 0.5, { opacity: 1 }, 0.05, "1");
+    time.staggerTo("#star41 > path", 0.5, { opacity: 0 }, 0.05, "0");
+    time.staggerTo("#star41 > path", 0.5, { opacity: 1 }, 0.05, "1");
+    time.staggerTo("#star42 > path", 0.5, { opacity: 0 }, 0.05, "0");
+    time.staggerTo("#star42 > path", 0.5, { opacity: 1 }, 0.05, "1");
 }
